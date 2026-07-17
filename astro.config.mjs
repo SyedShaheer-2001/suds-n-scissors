a42337d97c68e43da2512563512b528abe0b7b9c
cidevelopers <info@chameleon-ideas.com>
2026-07-17 20:48:10 +0500

    chore: change Astro build output to static for Netlify compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sanity from '@sanity/astro';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
+  output: 'static',
   integrations: [
     react(),
     sanity({
